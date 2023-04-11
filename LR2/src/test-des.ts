799aa0a36cb79cde324425879d7db957fdf5b2e1
Nikita <nikitosminsk100@gmail.com>
2023-04-11 13:22:11 +0300

    feat: LR3 - report

--- LR2/src/test-des.ts
+++ LR2/src/test-des.ts
@@ -2,14 +2,14 @@ import { DesService } from './DES/DesService';
 
 
 const testDESAlgorithm = (): void => {
-    const testMessage = 'Here\'s a message with spaces and LARGE TEXT';
+    const testMessage = 'Here\'s a message with spaces and LARGE TEXT 123';
     const testKey = 'some-advanced-key-2023';
 
     const encrypted: string = DesService.encrypt(testMessage, testKey);
     const decrypted = DesService.decrypt(encrypted, testKey);
     console.log('INITIAL MESSAGE: {', testMessage, '} INITIAL KEY: {', testKey, '}');
     console.log('ENCRYPTED: {', encrypted, '}');
-    //console.log('ENCRYPTED BYTES: ', [...encrypted].map(symbol => symbol.charCodeAt(0)));
+    console.log('ENCRYPTED BYTES: ', [...encrypted].map(symbol => symbol.charCodeAt(0)));
     console.log('DECRYPTED: {', decrypted, '}');
 };
 
